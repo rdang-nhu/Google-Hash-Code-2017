644242deb4395f3bc2b59c6fa732c8793cddc97a
X-Robot-2015 <raphael.dangnhu0@gmail.com>
2017-02-23 19:34:19 +0100

    Ajout de connexion

---
+++ src/Connexion.java
@@ -0,0 +1,14 @@
+
+public class Connexion {
+	Server S;
+	Endpoint E;
+	int latence;
+	
+	Connexion(Server server, Endpoint endpoint, int lat){
+		S = server;
+		E = endpoint;
+		latence = lat;
+	}
+	
+	
+}
