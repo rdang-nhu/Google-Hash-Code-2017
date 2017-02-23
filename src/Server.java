ab4f9b63455352b72be0b480874e19f22b2ee717
fandejuni <t.dardinier@gmail.com>
2017-02-23 20:17:39 +0100

    Added putvideo

--- src/Server.java
+++ src/Server.java
@@ -3,17 +3,20 @@ import java.util.*;
 public class Server {
     
     int id;
+    List<Video> videos;
     List<Connexion> connexions;
 
     public Server(int id) {
         this.id = id;
         this.connexions = new LinkedList<Connexion>();
+        this.videos = new LinkedList<Video>();
     }
 
     public void putVideo(Video video) {
         for (Connexion c: connexions) {
             c.endpoint.removeVideo(video);
         }
+        videos.add(video);
     }
 
 }
