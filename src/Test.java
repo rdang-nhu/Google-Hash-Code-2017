aaeadef5803324141c45d022cf2e1c87ca9e64e1
Gautier Izacard <gizacard@gmail.com>
2017-02-23 20:45:27 +0100

    no message

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,13 @@
 
 public class Test {
-	public void test1(){
+	public static void test1(){
 		Configuration c = new Configuration("me_at_the_zoo.in");
 		for(int i = 0; i < c.number_servers; i++){
-			
+			System.out.println(c.Servers[i].score());
 		}
+	}
+	
+	public static void main(String[] args) {
+		test1();
 	}
 }
