de0551026c78aebb8d9bf9071ea196d2a5b44ca9
X-Robot-2015 <raphael.dangnhu0@gmail.com>
2017-02-23 20:05:26 +0100

    OK

--- src/Server.java
+++ src/Server.java
@@ -3,11 +3,14 @@ import java.util.*;
 public class Server {
     
     int id;
+    int taille;
     List<Connexion> connexions;
 
-    public Server(int id) {
+    public Server(int id, int taille) {
         this.id = id;
+        this.taille = taille;
         this.connexions = new LinkedList<Connexion>();
     }
 
-}
+
+
--- src/Video.java
+++ src/Video.java
@@ -19,5 +19,5 @@ public class Video {
     	}
     	return score;
     }
-
+    
 }
